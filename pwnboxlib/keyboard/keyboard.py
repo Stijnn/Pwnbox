6092cce80be1079d6d75dcc90f1e98f6296ab642
Stijn Verhelpen <stijn.verhelpen@live.nl>
2022-05-30 12:24:28 +0200

    Immutable string bypass

--- pwnboxlib/keyboard/keyboard.py
+++ pwnboxlib/keyboard/keyboard.py
@@ -7,13 +7,13 @@ from keyboard.keytranslation import *
 class Keyboard():
     def __init__(self, gadget_path) -> None:
         if exists(gadget_path):
-            super().__init__({
+            self.__device_info__ = {
                 "gadget_path": gadget_path
-            })
+            }
         else:
-            super().__init__({
+            self.__device_info__ = {
                 "gadget_path": None
-            })
+            }
         self.gadget = None
         pass
 
@@ -49,12 +49,12 @@ class Keyboard():
 
     def __write_text__(self, text: str):
         for c in text:
-            b = self.__create_report__()
+            b = list(self.__create_report__())
             meta = USB_CHARACTER_TRANSLATION_KEYCODES.get(c)
             if meta[0]:
                 b[0] = chr(ord(b[0]) + KEY_MOD_LSHIFT)
             b[2] = chr(meta[1])
-            self.__write__(b)
+            self.__write__("".join(b))
             sleep(0.001)
             self.__write_zero_report__()
         pass
@@ -70,20 +70,20 @@ class Keyboard():
         sleep(0.001)
         self.__write_zero_report__()
         sleep(0.001)
-        enter_report    = self.__create_report__()
+        enter_report    = list(self.__create_report__())
         enter_report[2] = KEY_ENTER
-        self.write(enter_report)
+        self.write("".join(enter_report))
 
     
     def press_key(self, key: str, modifiers: List[str] = [KEY_NONE], release=True):
-        report = self.__create_report__()
+        report = list(self.__create_report__())
         
         for mod in modifiers:
             if len(mod) == 1:
                 report[0] = chr(ord(report[0]) + ord(mod))
         
         report[2] = key
-        self.__write__(report)
+        self.__write__("".join(report))
         if release:
             sleep(0.001)
             self.__write_zero_report__()
