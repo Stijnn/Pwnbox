cb7ae10c52323770eaa54566444157a2e3cc4561
Stijn Verhelpen <stijn.verhelpen@live.nl>
2022-06-01 17:30:13 +0200

    Logging

--- pwnboxlib/mass_storage/mass_storage.py
+++ pwnboxlib/mass_storage/mass_storage.py
@@ -1,4 +1,5 @@
 from pwnboxlib.proxydevice import ProxyDevice
+from pwnlogger import log, log_error, log_verbose, log_warning
 from os import system
 
 class StorageProxy(ProxyDevice):
@@ -8,12 +9,12 @@ class StorageProxy(ProxyDevice):
 
     
     def __load__(self):
-        system('mkdir -p ${' + self.image_location + '/img/d}')
-        system('mount -o loop,ro, -t vfat ' + self.image_location + ' ${' + self.image_location + '/img/d}')
-        system(f'mkdir -p functions/{self.device_name}')
-        system(f'echo 1 > functions/{self.device_name}/stall')
-        system(f'echo 0 > functions/{self.device_name}/lun.0/cdrom')
-        system(f'echo 0 > functions/{self.device_name}/lun.0/ro')
-        system(f'echo 0 > functions/{self.device_name}/lun.0/nofua')
-        system(f'echo {self.image_location} > functions/{self.device_name}/lun.0/file')
-        system(f'ln -s functions/{self.device_name} configs/c.1/')
+        log(system('mkdir -p ${' + self.image_location + '/img/d}'))
+        log(system('mount -o loop,ro, -t vfat ' + self.image_location + ' ${' + self.image_location + '/img/d}'))
+        log(system(f'mkdir -p functions/{self.device_name}'))
+        log(system(f'echo 1 > functions/{self.device_name}/stall'))
+        log(system(f'echo 0 > functions/{self.device_name}/lun.0/cdrom'))
+        log(system(f'echo 0 > functions/{self.device_name}/lun.0/ro'))
+        log(system(f'echo 0 > functions/{self.device_name}/lun.0/nofua'))
+        log(system(f'echo {self.image_location} > functions/{self.device_name}/lun.0/file'))
+        log(system(f'ln -s functions/{self.device_name} configs/c.1/'))
