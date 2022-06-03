1dad3e2abae39517e30ed3f606c3b13eabeeed5e
Stijn Verhelpen <stijn.verhelpen@live.nl>
2022-06-03 14:08:26 +0200

    Practical UI

---
+++ web/webapp.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, redirect, request, url_for
+
+app = Flask(__name__)
+
+
+script_links = {}
+script_count = 0
+for dir, sdir, file in os.walk('../scripts/usbrubberducky-payloads/payloads/library/'):
+    if 'payload.txt' in file:
+        fullpath = dir + '/' + 'payload.txt'
+        script_links[f'{script_count}:  {os.path.basename(dir)}'] = fullpath
+        script_count += 1
+
+
+
+@app.route('/exec', methods = ['POST'])
+def run_script():
+    if request.form['script']:
+        script_link = str(request.form['script'])
+        if script_link in script_links:
+            print(script_links[script_link])
+    return redirect('/')
+
+
+@app.route('/')
+def scripts():
+    html = '<ul>'
+    for link in dict(script_links).keys():
+        html += f'<li><form action="/exec" method="post"><input type="submit" value="{link}" name="script" /></form></li>'
+    html += '</ul>'
+    return html
+
+
+def main():
+    app.run()
+    pass
+
+
+if __name__ == '__main__':
+    main()
