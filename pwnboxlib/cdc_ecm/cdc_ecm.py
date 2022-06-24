1fb4e24761c35b27294d08d134f641899a3d1468
Stijn Verhelpen <stijn.verhelpen@live.nl>
2022-06-24 11:52:47 +0200

    netmasking

--- pwnboxlib/cdc_ecm/cdc_ecm.py
+++ pwnboxlib/cdc_ecm/cdc_ecm.py
@@ -3,9 +3,11 @@ from pwnboxlib.devicefactory import DeviceFactory
 from pwnlogger import log_command
 
 class EthernetFactory(DeviceFactory):
-    def __init__(self, device_name: str, local_mac: str = '42:63:65:65:43:21', remote_mac: str = '42:63:65:12:34:56') -> None:
+    def __init__(self, device_name: str, local_mac: str = '42:63:65:65:43:21', remote_mac: str = '42:63:65:12:34:56', ip: str = '192.168.4.1', netmask: str='255.255.255.255') -> None:
         self.local_mac = local_mac
         self.remote_mac = remote_mac
+        self.ip = ip
+        self.netmask = netmask
         super().__init__(device_name)
 
 
@@ -13,3 +15,4 @@ class EthernetFactory(DeviceFactory):
         log_command(f'mkdir -p functions/{self.device_name}')
         log_command(f'echo "{self.remote_mac}" > functions/{self.device_name}/host_addr')
         log_command(f'echo "{self.local_mac}" > functions/{self.device_name}/dev_addr')
+        log_command(f'ifconfig usb0 {self.ip} netmask {self.netmask} up')
